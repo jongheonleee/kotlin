28ad3fd8d59428cc2e6f2162ccede6b5be78f4e8
jongheonleee <qwefghnm1212@gmail.com>
2024-06-15 01:54:12 +0900

    별찍기 5번까지 해결

---
+++ src/main/kotlin/practice/별찍기/solve.kt
@@ -0,0 +1,83 @@
+package practice.별찍기
+
+fun main(args:Array<String>) {
+    quiz1()
+    quiz2()
+    quiz3()
+    quiz4()
+    quiz5()
+}
+
+fun quiz1() {
+    println("===quiz(1)===")
+    for (i in 0 .. 4) {
+        for (j in 0 .. 4) {
+            print('*');
+        }
+        println()
+    }
+    println("============")
+}
+
+fun quiz2() {
+    println("===quiz(2)===")
+    for (i in 0 .. 4) {
+        for (j in 0 .. 4) {
+            if (i + j < 4) {
+                print(" ")
+            } else if (i + j == 4) {
+                print('*')
+            }
+        }
+        println()
+    }
+    println("============")
+}
+
+fun quiz3() {
+    println("===quiz(3)===")
+    for (i in 0 .. 4) {
+        for (j in 0 .. 4) {
+            if (i >= j) {
+                print('*')
+            }
+        }
+        println()
+    }
+    println("============")
+}
+
+
+fun quiz4() {
+    val prefix : String = "*****"
+
+    println("===quiz(4)===")
+    for (i in 0 .. 4) {
+        print(prefix)
+        for (j in 0 .. 4) {
+            if (i >= j) {
+                print('*')
+            }
+        }
+        println()
+    }
+    println("============")
+}
+
+fun quiz5() {
+
+    println("===quiz(5)===")
+    for (i in 0 .. 4) {
+        for (j in 0 .. 4) {
+            if (i >= j && i >= 4 - j) {
+                print("*")
+            } else if (i > j || i > 4 - j){
+                print(" ")
+            } else {
+                print("*")
+            }
+        }
+        println()
+    }
+    println("============")
+}
