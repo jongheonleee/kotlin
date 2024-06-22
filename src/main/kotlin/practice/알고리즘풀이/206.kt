f6663cdbc739f60828849187e7215b47b3224f39
jongheonleee <qwefghnm1212@gmail.com>
2024-06-22 17:25:33 +0900

    206 수정

--- src/main/kotlin/practice/알고리즘풀이/206.kt
+++ src/main/kotlin/practice/알고리즘풀이/206.kt
@@ -11,29 +11,26 @@ class Solve_206 {
         while (curr != null) {
             arr.add(curr.`val`)
             curr = curr.next
+
         }
 
         // 배열 역정렬
         Collections.reverse(arr)
 
-        // LL 생성
-        // 반환
-        val head2 : ListNode?
-        var curr2 : ListNode?
+        // LL 생성 & 반환
+        return if (arr.size > 0) {
+            val head2 : ListNode
+            var curr2 : ListNode
 
-        if (arr.size > 0) {
             head2 = ListNode(arr[0])
             curr2 = head2
 
-            for (i in 1 .. arr.size) {
-                curr2?.next = ListNode(arr[i])
-                curr2 = curr2?.next
+            for (i in 1 .. arr.size-1) {
+                curr2.next = ListNode(arr[i])
+                curr2 = curr2.next!!
             }
 
             return head2
-        }
-
-        return null
-
+        } else null
     }
 }
